1ce6b0a7d84da2e11186fc6dd6247135c7f5fb44
Hrishikesh Mishra <sprt.email@gmail.com>
2017-08-13 19:09:50 +0530

    wait n notify

---
+++ src/main/java/com/hrishikeshmishra/jm/waitnnotify/App.java
@@ -0,0 +1,40 @@
+package com.hrishikeshmishra.jm.waitnnotify;
+
+/**
+ * @author hrishikesh.mishra
+ */
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        final Processor producer = new Processor();
+
+        Thread t1 = new Thread(() -> {
+
+            try {
+                producer.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+
+        Thread t2 = new Thread(() -> {
+
+            try {
+                producer.consumer();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+
+    }
+}
---
+++ src/main/java/com/hrishikeshmishra/jm/waitnnotify/Processor.java
@@ -0,0 +1,31 @@
+package com.hrishikeshmishra.jm.waitnnotify;
+
+import java.util.Scanner;
+
+/**
+ * @author hrishikesh.mishra
+ */
+public class Processor {
+
+    public void produce() throws InterruptedException {
+
+        synchronized (this){
+            System.out.println("Producer thread running.");
+            wait();
+            System.out.println("Resumed..");
+        }
+    }
+
+    public void consumer() throws InterruptedException {
+        Scanner in = new Scanner(System.in);
+
+        Thread.sleep(2000);
+
+        synchronized (this){
+            System.out.println("Waiting for return key..");
+            in.nextLine();
+            System.out.println("Return key pressed.");
+            notify();
+        }
+    }
+}
